17da8dc1e49cdcc383ca422b3547d67a60a9a3ea
zwikholm <kestrel@trylinux.us>
2016-03-24 15:03:08 -0500

    added whois

--- pyrdap_server.py
+++ pyrdap_server.py
@@ -1,6 +1,6 @@
 from flask import Flask,jsonify,session
 from ipwhois import IPWhois
-
+import requests
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
 
@@ -9,16 +9,44 @@ app.config["JSON_SORT_KEYS"] = False
 def get_rdap_ip(ip):
 	try:	
 		obj = IPWhois(ip)
-		results_raw = obj.lookup_rdap(depth=1)
+		results_raw = obj.lookup_rdap()
 		status = 200
 		results = jsonify(results_raw)
-	except:
+	except Exception as e:
+		print e
 		results_raw = jsonify({'status': "not_found"}) 
 		status = 404
 		results = jsonify({'status': "not_found"})
 	return results,status
 
+@app.route('/rdap/asn/<asn>', methods=['GET'])
+def get_rdap_asn(asn):
+	try:
+		url = 'http://hailey.opendnsbl.net:8080/rdapbootstrap/autnum/%s' % asn
+		r = requests.get(url)
+		status = 200
+		results = jsonify(r.json())
+	except Exception as e:
+		print e
+		results_raw = jsonify({'status': "not_found"})
+                status = 404
+                results = jsonify({'status': "not_found"})
+	return results,status
 
+@app.route('/whois/ip/<ip>', methods=['GET'])
+def get_whois_ip(ip):
+	try:
+		obj = IPWhois(ip)
+		results_raw = obj.lookup(get_referral=True)
+		status = 200
+		results = jsonify(results_raw)
+	
+	except Exception as e:
+                print e
+                results_raw = jsonify({'status': "not_found"})
+                status = 404
+                results = jsonify({'status': "not_found"})
+        return results,status
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8006,debug=True,processes=10)
